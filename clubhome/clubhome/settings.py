7fb201a836ff4ba6d6d659fab1e04ebb104b8da7
Yothin Muangsommuk <m@yothinix.com>
2021-03-28 17:47:58 +0700

    :wrench: Add CORS HEADERS support

--- clubhome/clubhome/settings.py
+++ clubhome/clubhome/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'users',
     'rooms',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,3 +133,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
